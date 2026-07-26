625da939b72f5e15da790e8190479b86c15d7b6b
MUKTAR ABDULWAHEED <mukhtarakanniabdulwaheed@gmail.com>
2026-07-26 10:27:36 +0100

    Update page.tsx

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -3,8 +3,17 @@ import SupportWidget from '@/components/SupportWidget';
 
 export default function WidgetPage() {
   return (
-    <main className="w-full h-full bg-transparent overflow-hidden">
-      <SupportWidget />
-    </main>
+    <>
+      {/* Force the iframe body to be completely transparent */}
+      <style dangerouslySetInnerHTML={{ __html: `
+        body, html { 
+          background-color: transparent !important; 
+          background: transparent !important; 
+        }
+      `}} />
+      <main className="w-full h-full bg-transparent overflow-hidden">
+        <SupportWidget />
+      </main>
+    </>
   );
 }
